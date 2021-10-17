40521192bf084878fe0fed8cfd86d405bfa63798
Natalie Ayers <nayers@uchicago.edu>
2021-10-17 12:21:30 -0500

    adding serial landsat calculation for 3a

---
+++ landsat_serial.py
@@ -0,0 +1,22 @@
+# Import required libraries
+import rasterio
+import numpy as np
+
+
+t0 = time.time()
+
+
+# Import bands as separate images; in /project2/macs30123 on Midway2
+band4 = rasterio.open(
+  '/project2/macs30123/landsat8/LC08_B4.tif') #red
+band5 = rasterio.open(
+  '/project2/macs30123/landsat8/LC08_B5.tif') #nir
+
+# Convert nir and red objects to float64 arrays
+red = band4.read(1).astype('float64')
+nir = band5.read(1).astype('float64')
+
+# NDVI calculation
+ndvi = (nir - red) / (nir + red)
+
+print('time elapsed for serial NDVI calculation:', time.time() - t0)
